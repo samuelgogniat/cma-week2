6e4ea47fb6ad71738b5e0e0da4b3df88acc0f4c9
samuel <samuelgogniat@outlook.com>
2023-05-01 10:05:59 +0200

    task1

--- week_2.R
+++ week_2.R
@@ -7,5 +7,8 @@ library(ggplot2)
 
 wildschwein <- read_delim("wildschwein_BE_2056.csv")
 
+## Task1
 
+wildschwein <- st_as_sf(wildschwein, coords = c("E", "N"), crs = 2056, remove = FALSE)
+wildschwein
 
